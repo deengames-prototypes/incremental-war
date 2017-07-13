d37c634926353c239e9b05e12704cbcc19aa4f49
ashes999 <ashes999@yahoo.com>
2017-07-13 18:14:42 -0400

    Ability to buy NEGs to generate enregy

--- code/source/deengames/incrementalwar/model/Region.hx
+++ code/source/deengames/incrementalwar/model/Region.hx
@@ -1,21 +1,40 @@
 package deengames.incrementalwar.model;
 
+import helix.data.Config;
+
 class Region
 {
     // Contains units, buildings (attack and defense), perhaps enemies, energy
 
     // Resources. Stored as floats but displayed as ints
-    public var alloyHarvested(default, null):Float = 0;
-    public var neodymiumHarvested(default, null):Float = 0;
-    public var energyHarvested(default, null):Float = 0;
+    public var numAlloy(default, null):Float = 0;
+    public var numNeodymium(default, null):Float = 0;
+    public var numEnergy(default, null):Float = 0;
 
     // Buildings.
     public var numNeodymiumElectricGenerators(default, null):Int = 0;
 
     public function new(startingAlloy:Int = 0, startingNeodymium:Int = 0)
     {
-        this.alloyHarvested = startingAlloy;
-        this.neodymiumHarvested = startingNeodymium;
+        this.numAlloy = startingAlloy;
+        this.numNeodymium = startingNeodymium;
+    }
+
+    // Returns true if bought, false if insufficient funds
+    public function buyNeydymiumElectricGenerator():Bool
+    {
+        var costs:Dynamic = Config.get("buildingCosts");
+        var buildingCosts:Array<Int> = costs.neodymiumElectricGenerator;
+        if (numAlloy >= buildingCosts[0] && numNeodymium >= buildingCosts[1])
+        {
+            // CHA-CHING!
+            this.numNeodymiumElectricGenerators++;
+            numAlloy -= buildingCosts[0];
+            numNeodymium -= buildingCosts[1];
+            return true;
+        }
+        
+        return false;
     }
 
     public function update(elapsedSeconds:Float):Void
--- code/source/deengames/incrementalwar/states/CoreGameState.hx
+++ code/source/deengames/incrementalwar/states/CoreGameState.hx
@@ -4,6 +4,7 @@ import deengames.incrementalwar.model.Earth;
 import deengames.incrementalwar.model.Region;
 import flixel.text.FlxText;
 import helix.core.HelixState;
+import helix.core.HelixText;
 
 class CoreGameState extends HelixState
 {
@@ -13,10 +14,17 @@ class CoreGameState extends HelixState
 	private var earth:Earth;
 	private var region:Region;
 
-	// UI elements
+	//// UI elements (display)
+	// resources
 	private var energyDisplay:FlxText;
 	private var alloyDisplay:FlxText;
 	private var neodymiumDisplay:FlxText;
+
+	// buildings
+	private var neodymiumElectricGeneratorsDisplay:FlxText;
+
+	//// UI elements (control)
+	private var buyNeodymiumElectricGenerator:HelixText;
 
 	override public function create():Void
 	{
@@ -43,6 +51,7 @@ class CoreGameState extends HelixState
 	private function createUi():Void
 	{
 		// Expectation: four digits max, eg. 1.1M, 1.7B
+		// UI: resources
 		this.energyDisplay = this.addText(0, UI_PADDING, "Energy: 9999", UI_FONT_SIZE);
 		this.energyDisplay.x = this.width - this.energyDisplay.width - UI_PADDING;
 
@@ -51,13 +60,33 @@ class CoreGameState extends HelixState
 
 		this.neodymiumDisplay = this.addText(0, Std.int(this.alloyDisplay.y + this.alloyDisplay.size + UI_PADDING), "Neodymium: 9999", UI_FONT_SIZE);
 		this.neodymiumDisplay.x = this.width - this.neodymiumDisplay.width - UI_PADDING;
+
+		// UI: buildings
+		this.neodymiumElectricGeneratorsDisplay = this.addText(UI_PADDING, 0, "NEGs: 0", UI_FONT_SIZE);
+		this.neodymiumElectricGeneratorsDisplay.y = this.height - this.neodymiumElectricGeneratorsDisplay.height - UI_PADDING;
 		
+		// UI: controls/buttons
+		// TODO: abbreviate to NEG with a mandatory pop-up expounding what it really is
+		this.buyNeodymiumElectricGenerator = this.addText(UI_PADDING, 0, "Buy NEG (Neodymium Electric Generator)", UI_FONT_SIZE);
+		this.buyNeodymiumElectricGenerator.y = this.neodymiumElectricGeneratorsDisplay.y - this.neodymiumElectricGeneratorsDisplay.height;
+		this.buyNeodymiumElectricGenerator.onClick(function()
+		{
+			var bought = this.region.buyNeydymiumElectricGenerator();
+			if (bought)
+			{
+				this.neodymiumElectricGeneratorsDisplay.text = 'NEGs: ${this.region.numNeodymiumElectricGenerators}';
+			}
+			else
+			{
+				// Play cancel sound
+			}
+		});
 	}
 
 	private function updateUi():Void
 	{
-		this.energyDisplay.text = 'Energy: ${Std.int(Math.floor(this.region.energyHarvested))}';
-		this.alloyDisplay.text = 'Alloy: ${Std.int(Math.floor(this.region.alloyHarvested))}';
-		this.neodymiumDisplay.text = 'Neodymium: ${Std.int(Math.floor(this.region.neodymiumHarvested))}';
+		this.energyDisplay.text = 'Energy: ${Std.int(Math.floor(this.region.numEnergy))}';
+		this.alloyDisplay.text = 'Alloy: ${Std.int(Math.floor(this.region.numAlloy))}';
+		this.neodymiumDisplay.text = 'Neodymium: ${Std.int(Math.floor(this.region.numNeodymium))}';
 	}
 }
